233e90fc92f191670bbcc1dd7835d1b1cfd813d0
cx9vc <cx9vc@mathcs02.richmond.edu>
2018-10-31 10:11:58 -0400

    update

--- LinkedList.h
+++ LinkedList.h
@@ -1,8 +1,9 @@
-fndef __LINKEDLIST_H__
+#ifndef __LINKEDLIST_H__
 #define __LINKEDLIST_H__
 
 #include <list>
 #include <vector>
+using namespace std;
 
 template <class T>
 class LinkedList
@@ -25,5 +26,68 @@ class LinkedList
 
      LinkedList<T>& operator+=( const T& item );
 };
+
+template <class T>
+LinkedList<T>::LinkedList()
+{
+    std::list<T> theList;
+}
+
+template <class T>
+LinkedList<T>::LinkedList(const LinkedList<T>& other)
+{
+    std::list<T> theList (other);
+}
+
+template <class T>
+LinkedList<T>::~LinkedList(){}
+
+template <class T>
+int LinkedList<T>::size() const
+{
+   return theList.size();
+}
+
+template <class T>
+void LinkedList<T>::add(T element)
+{
+    theList.push_back(element);
+}
+
+template <class T>
+T LinkedList<T>::get(int index) const
+{
+    std::list<T>::const_iterator it = theList.cbegin(); 
+    advance (it,index);
+    return *it;
+}
+
+template <class T>
+T LikedList<T>::remove(int index)
+{
+    std::list<T>const_iterator it = theList.cbegin();
+    adcance (it,index);
+    T temp = *it;
+    theList.erase(it);
+    return temp;
+
+}
+
+template <class T>
+std::vector<T> LinkedList<T>::toArray() const
+{
+    std::vecotr temp = 
+
+}
+
+template <class T>
+LinkedList<T>& LinkedList<T>::operator+=(const T& item){}
+
+
+
+
+
+
+
 
 #endif
